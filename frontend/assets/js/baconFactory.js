f045609161eb915de88f134622dda1ad61c7eef1
Dusan Petrovic <dule021@gmail.com>
2022-03-13 20:42:57 +0100

    Completed Bacon Factory (Task 1)

---
+++ frontend/assets/js/baconFactory.js
@@ -0,0 +1,48 @@
+let baconCount = 0;
+
+const baconHolder = document.querySelector(".bacon-holder");
+const initBacons = baconHolder.querySelectorAll(".bacon");
+const bacon = initBacons[0];
+
+const makeMoreBacon = () => {
+  baconCount++;
+  renderBacon(baconCount);
+};
+
+const reduceBacon = () => {
+  if (baconCount >= 1) {
+    baconCount--;
+  }
+
+  renderBacon(baconCount);
+};
+
+const renderBacon = (bacons) => {
+  while (baconHolder.firstChild) {
+    baconHolder.removeChild(baconHolder.firstChild);
+  }
+
+  for (let index = 0; index < bacons; index++) {
+    baconHolder.append(bacon.cloneNode());
+  }
+};
+
+const initBaconPage = () => {
+  const baconPage = document.querySelector(".bacon-page");
+
+  if (!baconPage) {
+    return;
+  }
+
+  baconCount = initBacons.length;
+
+  baconPage
+    .querySelector(".more-bacon-button")
+    .addEventListener("click", makeMoreBacon);
+
+  baconPage
+    .querySelector(".less-bacon-button")
+    .addEventListener("click", reduceBacon);
+};
+
+export default initBaconPage;
